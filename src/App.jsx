3d75d0f3b8f34d2484b8153020fd25f68c0cfcaa
yadavittal <vittalyadav8328@gmail.com>
2025-03-05 23:59:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+ 
+
+  return (
+    <div> 
+          <h1 style={{color:'blueviolet'}}>hello world</h1>
+          <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+function Counter() {
+  
+
+    const [count, setCount] = useState(0)
+
+    const handleClick = ()=>{
+          setCount(count+1)
+    }
+    const handleClick1 = ()=>{
+          setCount(count-1)
+    }
+
+  return (
+    <div>
+          <button onClick={handleClick}>Increment</button>
+          <p>{count}</p>
+          <button onClick={handleClick1}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter
